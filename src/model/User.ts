39b5fee2407ee4d85e24c5074f22d46b24f37088
Aditya Saini <adityasaini10012001@outlook.com>
2024-06-08 23:38:56 +0530

    added schemas and zod validation of signUpSchema
    Signed-off-by: Aditya Saini <adityasaini10012001@outlook.com>

--- src/model/User.ts
+++ src/model/User.ts
@@ -70,3 +70,7 @@ const UserSchema: Schema<User> = new Schema({
     
 
 })
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>('User', UserSchema);
+
+export default UserModel;
